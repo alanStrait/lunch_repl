f80af667c1401edbca6a3267cef34349e6615334
alanStrait <alans@brsg.io>
2023-02-09 15:42:40 -0700

    Initial commit post mix new

---
+++ lib/dine_outside.ex
@@ -0,0 +1,18 @@
+defmodule DineOutside do
+  @moduledoc """
+  Documentation for `DineOutside`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DineOutside.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dine_outside_test.exs
@@ -0,0 +1,8 @@
+defmodule DineOutsideTest do
+  use ExUnit.Case
+  doctest DineOutside
+
+  test "greets the world" do
+    assert DineOutside.hello() == :world
+  end
+end
